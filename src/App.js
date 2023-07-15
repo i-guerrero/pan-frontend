a01c39bcbfe0d21e9a47685e3366331329e700d3
i-guerrero <isaacguerrero@pursuit.org>
2023-07-15 17:18:57 -0400

    Add logo and navBar, favicon, and title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>PAN</h1>
     </div>
   );
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,9 +1,28 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
+import logo from "./pan-logo-cropped.png";
 import "./NavBar.css";
 
 export default function NavBar() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <Image className="logo" src={logo} fluid />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#link">Testimonials</Nav.Link>
+            <Nav.Link href="#home">Events</Nav.Link>
+            <Nav.Link href="#home">Partners</Nav.Link>
+            <Nav.Link href="#home">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
