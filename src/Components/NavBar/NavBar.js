ad074d9699648b0a40cecdbea9c71c8041396062
i-guerrero <isaacguerrero@pursuit.org>
2023-07-15 17:20:55 -0400

    Update ref links in navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -15,11 +15,11 @@ export default function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">About</Nav.Link>
-            <Nav.Link href="#link">Testimonials</Nav.Link>
-            <Nav.Link href="#home">Events</Nav.Link>
-            <Nav.Link href="#home">Partners</Nav.Link>
-            <Nav.Link href="#home">Contact</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#testimonials">Testimonials</Nav.Link>
+            <Nav.Link href="#events">Events</Nav.Link>
+            <Nav.Link href="#partners">Partners</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
