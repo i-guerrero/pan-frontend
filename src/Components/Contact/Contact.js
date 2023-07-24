07c1ddde852dae74aca9e46780cb14f056300eb1
Isaac Guerrero <isaac.guerrero@accenture.com>
2023-07-24 19:50:26 -0400

    Add react-icons package to app and app contact info in footer

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,12 +1,42 @@
 import Container from "react-bootstrap/Container";
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
 import "./Contact.css";
+import logo from "../../pan-logo.png"
+import { BsFacebook, BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs"
 
 export default function Contact() {
   return (
     <div id="contact">
       {/* add photo */}
+      
       <Container>
-        <h1>Contact</h1>
+        <Row>
+          <Col xs={6} md={4}>
+            <Image className="logo-contact" src={logo} alt="logo" />
+          </Col>
+          <Col xs={6} md={4}>
+            <h3>Contact Us!</h3>
+            <p>+1 123-123-1234 <br />
+            info.contact@philadephiaapprenticenetork.com<br />
+            17 Philadephia Ave, Philadephia, PA 01234</p>
+            <Row>
+              <Col>
+                <BsFacebook />
+              </Col>
+              <Col>
+                <BsTwitter />
+              </Col>
+              <Col>
+                <BsInstagram />
+              </Col>
+              <Col>
+                <BsLinkedin />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
